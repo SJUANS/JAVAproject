12051615a3f954164e2f17bcbea391516fa7aeb9
SJUANS <lovejuan95@hanmail.net>
2022-06-24 18:05:41 +0900

    인텔리제이 단축키 연습: psvm, soutv

---
+++ src/Shortcuts.java
@@ -0,0 +1,6 @@
+// 인텔리제이 단축키 사용하기: psvm, soutv
+public class Shortcuts {
+    public static void main(String[] args) {
+        System.out.println("IntelliJ shortcuts are very intelligent");
+    }
+}
